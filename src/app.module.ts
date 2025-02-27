e1361ee2380bf0388f1cf41f167396f2f2e57aae
Nikita <ragerage333@gmail.com>
2025-02-27 05:16:11 +0300

    3.00

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+// import { AuthModule } from './auth/auth.module'
+// import { EmailConfirmationModule } from './auth/email-confirmation/email-confirmation.module'
+// import { PasswordRecoveryModule } from './auth/password-recovery/password-recovery.module'
+// import { ProviderModule } from './auth/provider/provider.module'
+// import { TwoFactorAuthModule } from './auth/two-factor-auth/two-factor-auth.module'
+import { IS_DEV_ENV } from './libs/common/utils/is-dev.util'
+// import { MailModule } from './libs/mail/mail.module'
+
+// import { UserModule } from './user/user.module'
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+	imports: [
+		ConfigModule.forRoot({
+			ignoreEnvFile: !IS_DEV_ENV,
+			isGlobal: true
+		}),
+		PrismaModule,
+		AuthModule,
+		UserModule,
+		
+		// AuthModule,
+		// UserModule,
+		// ProviderModule,
+		// MailModule,
+		// EmailConfirmationModule,
+		// PasswordRecoveryModule,
+		// TwoFactorAuthModule
+	]
+})
+export class AppModule {}
